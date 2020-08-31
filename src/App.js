93793589a936d3dfa4f2bfe83aaf2e679118970c
Mu7ammad Abed <mu7ammadabed@gmail.com>
2020-08-31 18:30:17 +0300

    Fetch data inside componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to my new project</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    monsters: [],
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((result) => this.setState({ monsters: result }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to my new project</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
