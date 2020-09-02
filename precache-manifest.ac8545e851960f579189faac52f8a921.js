dd0c24af0d73c12c1c9c7b0b03a5fec18a170d59
Mu7ammad Abed <mu7ammadabed@gmail.com>
2020-09-02 19:20:12 +0300

    Updates

---
+++ precache-manifest.ac8545e851960f579189faac52f8a921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3371e9d4c6979bf149ff4dcbf4f0bd6d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d363ea28a07573f209ad",
+    "url": "/monsters-rolodex/static/css/main.36db90a3.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d363ea28a07573f209ad",
+    "url": "/monsters-rolodex/static/js/main.de1bcd30.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
